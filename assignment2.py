4ff8ebc207f6ea71f017d6a4eeeb2983ab2bbc3d
sarahmakumi <makumisarah2001@gmail.com>
2025-04-05 21:37:13 +0300

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,19 @@
+my_list = [10, 20, 30, 40]
+my_list.insert(1, 15)
+print (my_list)
+
+#extending the list
+my_list.extend([50, 60, 70])
+print (my_list)
+
+#deleting the last item
+my_list.pop(-1)
+print (my_list)
+
+#sorting the list in ascending orrder 
+my_list.sort()
+print (my_list)
+
+# printing the index of 30
+index = my_list.index(30)
+print (index)
